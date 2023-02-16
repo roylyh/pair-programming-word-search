797bc55513b5fa089e7034713384d046191bf91a
roylyh <roy.louyihui@gmail.com>
2023-02-15 23:46:41 -0800

    pair programming with Cloris

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let array = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) array.push([]);
+      array[j][i] = matrix[i][j];
+    }
+  }
+  return array;
+};
+
+
+module.exports = wordSearch;
